afee6387757293f413921f5a0b736470d53e6534
Mark Rushton <mark@modernfidelity.co.uk>
2013-04-28 18:18:05 +0100

    Started routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* App Module */
+
+angular.module('approutes', []).
+  config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider.
+      when('/portfolio', {templateUrl: 'partials/portfolio.html',   controller: PortfolioListCtrl}).
+  	  when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      //when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      otherwise({redirectTo: '/'});
+
+
+}]);
